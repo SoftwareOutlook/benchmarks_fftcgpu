ff838300c5ae9d898a87368f8a0b2b0f4fe7220f
pgambron <p.gambron@tutanota.com>
2019-03-27 14:22:44 +0000

    glados

--- glados/runbenchmarks.sh
+++ glados/runbenchmarks.sh
@@ -1,32 +1,43 @@
 #!/bin/bash
 
+export OMP_PROC_BIND=spread
+
+#PROG=/home/pgambron/GPU/cuda/test
+#PROG=/home/pgambron/GPU/kokkos/test
+#PROG=/home/pgambron/GPU/opencl/test
+#PROG=/home/pgambron/GPU/openacc/test
+PROG=/home/pgambron/GPU/openmp/test
+
+echo "===================================="
+echo $PROG
+echo
 
 # CCP PET/MR
 # N ~ 256, N_COILS ~ 32
 
 # power of 2
-qsub -v N_X=256,N_Y=256,N_Z=256,N_COILS=32 benchmark.sh 
+$PROG 256 256 256 32 
 
 # product of small primes 2*3*5*7=210
-qsub -v N_X=210,N_Y=210,N_Z=210,N_COILS=32 benchmark.sh 
+$PROG 210 210 210 32 
 
 # prime numbers 257 
-qsub -v N_X=257,N_Y=257,N_Z=257,N_COILS=32 benchmark.sh 
+$PROG 257 257 257 32 
 
 
 
 # Regular tests
 # powers of 2
 # square
-qsub -v N_X=128,N_Y=128,N_Z=128,N_COILS=1 benchmark.sh
-qsub -v N_X=256,N_Y=256,N_Z=256,N_COILS=1 benchmark.sh
-qsub -v N_X=512,N_Y=512,N_Z=512,N_COILS=1 benchmark.sh  
+$PROG 128 128 128 1 
+$PROG 256 256 256 1 
+$PROG 512 512 512 1   
 
 #flattened
 # ratio 16
-qsub -v N_X=32768,N_Y=8,N_Z=8,N_COILS=1 benchmark.sh
-qsub -v N_X=65536,N_Y=16,N_Z=16,N_COILS=1 benchmark.sh
-qsub -v N_X=131072,N_Y=32,N_Z=32,N_COILS=1 benchmark.sh  
+$PROG 32768 8 8 1 
+$PROG 65536 16 16 1 
+$PROG 131072 32 32 1   
 
 
 #product of small integers
@@ -41,23 +52,23 @@ qsub -v N_X=131072,N_Y=32,N_Z=32,N_COILS=1 benchmark.sh
 #2 2 3 1   31500
 
 #square
-qsub -v N_X=210,N_Y=210,N_Z=210,N_COILS=1 benchmark.sh
-qsub -v N_X=420,N_Y=420,N_Z=420,N_COILS=1 benchmark.sh
+$PROG 210 210 210 1 
+$PROG 420 420 420 1 
 
 #flattened
 #ratio 2*5=14
-qsub -v N_X=41160,N_Y=15,N_Z=15,N_COILS=1 benchmark.sh
-qsub -v N_X=82320,N_Y=30,N_Z=30,N_COILS=1 benchmark.sh
+$PROG 41160 15 15 1 
+$PROG 82320 30 30 1 
 
 
 # primes
 # 11 13 127 257 509 1021 2053 4093 8191 16381 32771
 # square
-qsub -v N_X=127,N_Y=127,N_Z=127,N_COILS=1 benchmark.sh
-qsub -v N_X=257,N_Y=257,N_Z=257,N_COILS=1 benchmark.sh
-qsub -v N_X=509,N_Y=509,N_Z=509,N_COILS=1 benchmark.sh  
+$PROG 127 127 127 1 
+$PROG 257 257 257 1 
+$PROG 509 509 509 1   
 
 # flattened
-qsub -v N_X=32771,N_Y=7,N_Z=7,N_COILS=1 benchmark.sh
-qsub -v N_X=65537,N_Y=17,N_Z=17,N_COILS=1 benchmark.sh
-qsub -v N_X=131071,N_Y=31,N_Z=31,N_COILS=1 benchmark.sh
+$PROG 32771 7 7 1 
+$PROG 65537 17 17 1 
+$PROG 131071 31 31 1 
