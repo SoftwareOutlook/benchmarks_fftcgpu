1814a5a72dfdd88126d82a0f6892f8c885b91aa6
Philippe <p.gambron@tutanota.com>
2019-05-03 10:42:55 +0100

    script

---
+++ archer/gatherresults.py
@@ -0,0 +1,68 @@
+import sys
+
+if len(sys.argv)<=1:
+  exit()
+
+filename=sys.argv[1]
+
+f=open(filename, "r")
+lines=f.readlines();
+f.close()
+
+keys_list=[]
+parameters_list=[]
+parameter=int(0)
+
+for line in lines:
+  if len(line)<1:
+    continue
+  if line[0]!="@":
+    continue
+  elements=line.split()
+  key=elements[1]
+  parameter=elements[2]
+  keys_list.append(key)
+  parameters_list.append(parameter)
+
+keys_list_unique=set(keys_list)
+parameters_list_unique=set(parameters_list)
+
+data={}
+averages={}
+for key in keys_list_unique:
+  data[key]={}
+  averages[key]={}
+  for parameter in parameters_list_unique:
+    data[key][parameter]=[];
+    averages[key][parameter]=0
+
+for line in lines:
+  if len(line)<1:
+    continue
+  if line[0]!="@":
+    continue
+  elements=line.split()
+  key=elements[1]
+  parameter=int(elements[2])
+  result=float(elements[3])
+  data[key][parameter].append(result)
+ 
+for key in data:
+  for parameter in data[key]:
+    n=0
+    for result in data[key][parameter]:
+      n=n+1
+      averages[key][parameter]=averages[key][parameter]+result     
+    averages[key][parameter]=averages[key][parameter]/n
+
+print '# N',
+for key in keys_list_unique:
+  print key,
+print ''
+
+for parameter in parameters_list_unique:
+  print parameter, 
+  for key in keys_list_unique:
+     print averages[key][parameter],    
+  print ''
+
